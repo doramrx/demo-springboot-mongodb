950fcff822f6f7aa8cf56b2b4b2d8b459f93667f
Isadora <isah3027@gmail.com>
2023-11-19 17:56:01 -0300

    Endpoint to return a user's posts

--- src/main/java/com/project/demospringbootmongodb/resource/UserResource.java
+++ src/main/java/com/project/demospringbootmongodb/resource/UserResource.java
@@ -1,5 +1,6 @@
 package com.project.demospringbootmongodb.resource;
 
+import com.project.demospringbootmongodb.domain.Post;
 import com.project.demospringbootmongodb.domain.User;
 import com.project.demospringbootmongodb.dto.UserDTO;
 import com.project.demospringbootmongodb.services.UserService;
@@ -55,5 +56,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
